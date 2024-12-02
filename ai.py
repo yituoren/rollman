b86777331a51fb66bb7eb65b20d102f7372b24b6
zyc <1455854619@qq.com>
2024-12-02 11:17:56 +0800

    fixed: altered main format

--- ai.py
+++ ai.py
@@ -1,4 +1,58 @@
-from main import *
+from core.gamedata import GameState
 
-def ai(game_state: GameState) -> list[list[int]]:
-    pass
+
+def example_ai(game_state: GameState) -> list[int]:
+    import numpy as np
+
+    ghost_pos = game_state.ghosts_pos
+    pacman_pos = game_state.pacman_pos
+
+    vector1 = np.array(ghost_pos[0]) - np.array(pacman_pos)
+    standardize1 = vector1 / np.linalg.norm(vector1)
+
+    vector2 = np.array(ghost_pos[1]) - np.array(pacman_pos)
+    standardize2 = vector2 / np.linalg.norm(vector2)
+
+    vector3 = np.array(ghost_pos[2]) - np.array(pacman_pos)
+    standardize3 = vector3 / np.linalg.norm(vector3)
+
+    ret = []
+    if abs(standardize1[0]) > abs(standardize1[1]):
+        if standardize1[0] > 0:
+            ret.append(2)
+        else:
+            ret.append(4)
+    else:
+        if standardize1[1] > 0:
+            ret.append(3)
+        else:
+            ret.append(1)
+            
+    if abs(standardize2[0]) > abs(standardize2[1]):
+        if standardize2[0] > 0:
+            ret.append(2)
+        else:
+            ret.append(4)
+    else:
+        if standardize2[1] > 0:
+            ret.append(3)
+        else:
+            ret.append(1)
+            
+    if abs(standardize3[0]) > abs(standardize3[1]):
+        if standardize3[0] > 0:
+            ret.append(2)
+        else:
+            ret.append(4)
+    else:
+        if standardize3[1] > 0:
+            ret.append(3)
+        else:
+            ret.append(1)
+
+    return ret
+
+
+
+ai_func = example_ai
+__all__ = ['ai_func']
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 
 from ai_to_judger import pacman_to_judger
 from ai_to_judger import ghost_to_judger
-from ai import ai
+from ai import *
 from core.GymEnvironment import PacmanEnv
 from core.gamedata import GameState
 from utils.utils import write_to_judger
@@ -70,4 +70,4 @@ class Controller:
 if __name__ == "__main__":
     print("init done", file=sys.stderr)
     controller = Controller()
-    controller.run(ai)
+    controller.run(ai_func)
